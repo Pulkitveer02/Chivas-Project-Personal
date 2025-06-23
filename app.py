56726182116de152d3bd39dd52aba6961e6182c0
Pulkitveer02 <83143895+Pulkitveer02@users.noreply.github.com>
2025-06-23 21:55:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ df = pd.read_parquet("datasets\player_stats.parquet")
 df_formations = df.loc[(df['type'] == "formationPlace") & (df['value'] > 0)]
 df_formations["pos"]  = [pos[0] for pos in df_formations["pos"]]
 
-df_team_formations = pd.read_parquet("datasets\formations (2).parquet")
+df_team_formations = pd.read_parquet("datasets\formations.parquet")
 
 
 # STEP 1: Get Most Common Formation Per Team
